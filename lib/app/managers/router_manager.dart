91c31dbed2040da36de8ed1fb2bfdc1f71f9d6f8
Mina Nashaat <72265427+Mina-Nashaat10@users.noreply.github.com>
2024-07-15 20:58:17 +0300

    add app screens to routes

--- lib/app/managers/router_manager.dart
+++ lib/app/managers/router_manager.dart
@@ -2,10 +2,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:route_tech_task/app/dependencies_injection.dart';
+import 'package:route_tech_task/presentation/common/screens/splash/splash_page.dart';
+import 'package:route_tech_task/presentation/user/screens/products/products_screen.dart';
 
 class RouterManager {
 
-  Map<String, Widget Function(BuildContext)> get routes => {};
+  Map<String, Widget Function(BuildContext)> get routes => {
+    SplashScreen.path   : (context) => const SplashScreen(),
+    ProductsScreen.path : (context) => const ProductsScreen(),
+  };
 
   bool get canPop => instance<GlobalKey<NavigatorState>>().currentState!.canPop();
 }
